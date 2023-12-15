a637b28985d28fe4487e3d62b241da376ffd921e
Thibaux Swildens <thibaux.swildens@gmail.com>
2023-12-15 14:42:58 +0100

    Copied routes

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,14 +1,114 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import SpotifyWebApi from "spotify-web-api-node"
+const app = express()
 
-const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const app = express();
+// app.get('/', (req, res) => {
+//   res.send({ message: 'Hello API' });
+// });
+//
+// app.listen(port, host, () => {
+//   console.log(`[ ready ] http://${host}:${port}`);
+// });
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+let sdk;
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
-});
+  res.send('Hello World!')
+})
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
-});
+app.post("/login", async (req, res) => {
+  const { code } = req.body
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  })
+
+  try {
+    const {
+      body: { access_token, refresh_token, expires_in },
+    } = await spotifyApi.authorizationCodeGrant(code)
+
+    res.json({ access_token, refresh_token, expires_in })
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(400)
+  }
+})
+
+app.post("/refresh", async (req, res) => {
+  const { refreshToken } = req.body
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  })
+
+  try {
+    const {
+      body: { access_token, expires_in },
+    } = await spotifyApi.refreshAccessToken()
+    res.json({ access_token, expires_in })
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(400)
+  }
+})
+
+// app.get("/lyrics", async (req, res) => {
+//   const { artist, track } = req.query
+//   const lyrics = (await lyricsFinder(artist, track)) || "No Lyrics Found"
+//   res.json({ lyrics })
+// })
+
+// app.get('/login', (req, res) => {
+//     const api = SpotifyApi.withUserAuthorization(
+//         "331d502b9fb242c8b7a1cb8b2ae23f98",
+//         "http://localhost:8080/auth",
+//     );
+//
+//     api.currentUser.profile();
+// })
+
+// app.post('/auth', (req, res) => {
+//     let data = req.body;
+//     sdk = SpotifyApi.withAccessToken("331d502b9fb242c8b7a1cb8b2ae23f98", data);
+//
+//     res.redirect('http://localhost:3000');
+// });
+//
+// const APIController = async () => {
+//     const result = await spotifyApi.clientCredentialsGrant({
+//         client_id: "331d502b9fb242c8b7a1cb8b2ae23f98",
+//         client_secret: "a0fcd7a90430488f9b25ae6551ff15b5"
+//     });
+//
+//     console.log(result);
+//     return result;
+// }
+//
+// app.get('/beatles', () => APIController())
+
+// app.get('/beatles', (req, res) => {
+//     sdk = SpotifyApi.withClientCredentials("331d502b9fb242c8b7a1cb8b2ae23f98", "a0fcd7a90430488f9b25ae6551ff15b5");
+//
+//     const getSearch = async () => {
+//         return await sdk.search("The Beatles", ["artist"]);
+//     }
+//
+//     const i = getSearch();
+//
+//     res.send(i);
+// });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
