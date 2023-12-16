0497a7cbdec2acb6a94ba1ca27d91cb08dd8576f
Thibaux Swildens <thibaux.swildens@gmail.com>
2023-12-16 17:01:18 +0100

    fix tha shiet

--- apps/music-quiz/src/app/Login.tsx
+++ apps/music-quiz/src/app/Login.tsx
@@ -5,14 +5,18 @@ export const Login = () => {
 
 	const clientId = "331d502b9fb242c8b7a1cb8b2ae23f98";
 	const redirectUrl = "http://localhost:4200/home";
+	const scopes = "streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+	const url = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUrl}&scope=${scopes}&show_dialog=true`;
 
-	const url = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUrl}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+	const handleClick = () => {
+		window.location.href = url;
+	}
 
 	return (
 		<div>
-			<a href={url}>
+			<div onClick={handleClick}>
 				<button>click hrere</button>
-			</a>
+			</div>
 			bitches
 		</div>
 	)
