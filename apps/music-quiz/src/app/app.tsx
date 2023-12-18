aa549cdbe75ca958dfbd2c5c52afb1484db2ec05
barbarastor <barbara@piggy.nl>
2023-12-18 09:05:23 +0100

    rebase

--- apps/music-quiz/src/app/app.tsx
+++ apps/music-quiz/src/app/app.tsx
@@ -4,6 +4,17 @@ import {Login} from "./Login";
 import {Home} from "./Home";
 import {Start} from "./Start";
 import {Playlists} from "./Playlists";
+import WelcomeComponent from "../../../../ui/src/lib/Components/welcomeComponent";
+import { Ui } from "@music-quiz/ui";
+
+
+function Welcome() {
+  return <WelcomeComponent/>
+}
+
+function Ui2() {
+  return <Ui/>
+}
 
 const App = () => {
     return (
@@ -12,6 +23,9 @@ const App = () => {
             <Route path="/home" element={<Home/>}/>
             <Route path="/start" element={<Start/>}/>
             <Route path="/playlists" element={<Playlists/>}/>
+
+            <Route path={"/welcome"} element={Welcome()}></Route>
+            <Route path={"/ui"} element={Ui2()}></Route>
         </Routes>
     );
 };
