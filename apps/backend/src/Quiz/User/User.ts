120ad0704e652acb54bc97643136c18245bc6c71
Thibaux Swildens <thibaux.swildens@gmail.com>
2023-12-23 15:52:40 +0100

    work

--- apps/backend/src/Quiz/User/User.ts
+++ apps/backend/src/Quiz/User/User.ts
@@ -1,9 +1,11 @@
 type UserType = {
-	user_id: string | number;
+	user_id: string | number | null;
+	name: string | null;
 }
 
 const User = <UserType>{
-	user_id: null
+	user_id: null,
+	name: null
 }
 
 export default User;
