5fcd5ab179d9f5f9b2bd2bcd42c32bd609f5c873
Thibaux Swildens <thibaux.swildens@gmail.com>
2024-01-03 16:48:38 +0100

    Reverts

--- apps/music-quiz/src/app/Domains/Auth/Login.tsx
+++ apps/music-quiz/src/app/Domains/Auth/Login.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 export const Login = () => {
-    const scopes =
-        'streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
-    const url = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=${scopes}&show_dialog=true`;
+    const url = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&state=1234567890&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state&show_dialog=true`;
 
     return (
         <div>
@@ -11,6 +9,7 @@ export const Login = () => {
                 <button>click hrere</button>
             </div>
             bitches
+            {url}
         </div>
     );
 };
