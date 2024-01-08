0233dbccafc1bdea2c3ac8b4ecd33b2c1f1cac55
Thibaux Swildens <thibaux.swildens@gmail.com>
2024-01-08 23:25:57 +0100

    Ui fixiee

--- apps/music-quiz/src/Layout/Page/PageContainer.tsx
+++ apps/music-quiz/src/Layout/Page/PageContainer.tsx
@@ -4,7 +4,7 @@ export const PageContainer = ({ children }: { children: React.ReactNode }) => {
     return (
         <div
             className={
-                'flex sm:flex-row flex-col lg:w-full w-10/12 m-auto sm:items-start items-center sm:justify-center justify-start pt-40'
+                'flex sm:flex-row flex-col lg:w-full w-10/12 m-auto sm:items-start items-center sm:justify-center justify-start pt-40 gap-20'
             }
         >
             {children}
