b2a2f71150ad5644eb95641730287e3c10bf51ab
Thibaux Swildens <thibaux.swildens@gmail.com>
2024-01-14 21:47:06 +0100

    Improved ui

--- apps/music-quiz/src/Layout/Page/PageContainer.tsx
+++ apps/music-quiz/src/Layout/Page/PageContainer.tsx
@@ -4,7 +4,7 @@ export const PageContainer = ({ children }: { children: React.ReactNode }) => {
     return (
         <div
             className={
-                'flex sm:flex-row flex-col lg:w-full w-10/12 h--full m-auto sm:items-start items-center sm:justify-center justify-start gap-20 pt-40'
+                'flex sm:flex-wrap sm:flex-row flex-col lg:w-full w-10/12 h-[40rem] m-auto sm:items-start items-center sm:justify-center justify-start md:gap-10 gap-10 px-10 pb-10 pt-40'
             }
         >
             {children}
